dda79bc714f6b25dde5393b3c7a4f39bbc36623f
xyph9r <79531792+xyph9r@users.noreply.github.com>
2024-11-19 11:31:05 -0500

    Delete Discount.py

--- Discount.py
+++
@@ -1,8 +0,0 @@
-
-original_price = float(input("Enter the item's original price: "))
-
-discount = 0.2 * original_price
-
-sale_price = original_price - discount
-
-print("The sale price is", sale_price)
